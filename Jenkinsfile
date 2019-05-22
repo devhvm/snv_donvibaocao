4722a928c9237be22adbaa8fe2c8c0aa303b63dc
VuNT <nguyenthachvu.vn@gmail.com>
2019-05-22 07:30:44 +0700

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,69 @@
+#!/usr/bin/env groovy
+
+node {
+    stage('Checkout') {
+        git url: 'https://github.com/devhvm/snv_phathanhmaubaocao.git', branch: 'development'
+    }
+    stage("build") {
+        docker.image("jhipster/jhipster:v5.8.2").inside("-v /home/vunt/maven/.m2:/root/.m2") { c ->
+            stage('check java') {
+                sh "java -version"
+            }
+
+            stage('clean') {
+                sh "chmod +x mvnw"
+                sh "./mvnw clean"
+            }
+
+            // stage('backend tests') {
+            //     try {
+            //         sh "./mvnw test"
+            //     } catch(err) {
+            //         throw err
+            //     } finally {
+            //         junit '**/target/surefire-reports/TEST-*.xml'
+            //     }
+            // }
+
+            stage('packaging') {
+                sh "./mvnw verify -Pprod -DskipTests"
+                archiveArtifacts artifacts: '**/target/*.war', fingerprint: true
+            }
+        }
+    }
+
+    def dockerImage
+    stage('build docker') {
+        sh "cp -R src/main/docker target/"
+        sh "cp target/*.war target/docker/"
+        echo 'I only execute on the development branch'
+        dockerImage = docker.build('snv-development/service-phathanhmaubaocao', 'target/docker')
+    }
+    stage ('Run') {
+
+        sh "cp -R src/main/docker target/"
+        sh "cp target/*.war target/docker/"
+        //sh "docker system prune -a -f --volumes"
+
+        echo 'I only execute on the development branch'
+        try {
+            sh '( docker stop phathanhmaubaocao-app-dev > /dev/null && echo Stopped container phathanhmaubaocao-app-dev && \
+  docker rm phathanhmaubaocao-app-dev ) 2>/dev/null || true'
+        } catch(err) {
+
+        }
+        docker.image("snv-development/service-phathanhmaubaocao").run('--name phathanhmaubaocao-app-dev --net network-snv-dev --env-file target/docker/env_dev.list')
+
+
+    }
+    // stage ('Run') {
+    //     sh "cp -R src/main/docker target/"
+    //     sh "cp target/*.war target/docker/"
+    //     echo 'I only execute on the development branch'
+    //     sh "docker-compose -f docker-compose-dev.yml down"
+    //     sh "docker-compose -f docker-compose-dev.yml  build"
+    //     sh "docker-compose -f docker-compose-dev.yml  up -d"
+    // }
+
+
+}
---
+++ src/main/java/com/manager/donvibaocao/service/dto/SaveBaoCaoDTO.java
@@ -0,0 +1,125 @@
+package com.manager.donvibaocao.service.dto;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.manager.donvibaocao.domain.DuLieuBaoCao;
+import com.manager.donvibaocao.domain.TienTrinhBaoCao;
+import com.manager.donvibaocao.domain.enumeration.Status;
+
+/**
+ * A DTO for the BaoCao entity.
+ */
+public class BaoCaoDTO implements Serializable {
+
+    private String id;
+
+    @NotNull
+    private String baoCaoCode;
+
+    @NotNull
+    private String mauBaoCaoCode;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Status status;
+
+    @NotNull
+    private List<DuLieuBaoCaoDTO> dulieubaocaos = new ArrayList<>();
+
+    @NotNull
+    private List<TienTrinhBaoCaoDTO> tientrinhbaocaos = new ArrayList<>();
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBaoCaoCode() {
+        return baoCaoCode;
+    }
+
+    public void setBaoCaoCode(String baoCaoCode) {
+        this.baoCaoCode = baoCaoCode;
+    }
+
+    public String getMauBaoCaoCode() {
+        return mauBaoCaoCode;
+    }
+
+    public void setMauBaoCaoCode(String mauBaoCaoCode) {
+        this.mauBaoCaoCode = mauBaoCaoCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<DuLieuBaoCaoDTO> getDulieubaocaos() {
+        return dulieubaocaos;
+    }
+
+    public void setDulieubaocaos(List<DuLieuBaoCaoDTO> dulieubaocaos) {
+        this.dulieubaocaos = dulieubaocaos;
+    }
+
+    public List<TienTrinhBaoCaoDTO> getTientrinhbaocaos() {
+        return tientrinhbaocaos;
+    }
+
+    public void setTientrinhbaocaos(List<TienTrinhBaoCaoDTO> tientrinhbaocaos) {
+        this.tientrinhbaocaos = tientrinhbaocaos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        BaoCaoDTO baoCaoDTO = (BaoCaoDTO) o;
+        if (baoCaoDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), baoCaoDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "BaoCaoDTO{" +
+            "id=" + getId() +
+            ", baoCaoCode='" + getBaoCaoCode() + "'" +
+            ", mauBaoCaoCode='" + getMauBaoCaoCode() + "'" +
+            ", name='" + getName() + "'" +
+            ", status='" + getStatus() + "'" +
+            "}";
+    }
+}
